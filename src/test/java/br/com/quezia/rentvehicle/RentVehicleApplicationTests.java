7fdc24841a3cc2eaf56eff80934a3fcd73d948d5
queziaqueiroz <kezyantunes@gmail.com>
2023-01-17 16:27:31 -0300

    estrutura inicial do projeto

---
+++ src/test/java/br/com/quezia/rentvehicle/RentVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quezia.rentvehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
